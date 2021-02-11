c04a01e0b248e29c2562db22df6ca373b82b40ce
naldurien <nicole.aldurien@gmail.com>
2021-02-11 13:03:29 -0500

    continued work

--- pool_table_class.py
+++ pool_table_class.py
@@ -1,7 +1,6 @@
 #WEEK 2 DAYS 3-5: POOL TABLE APP
 
 import datetime
-import pickle
 
 class PoolTable:
     def __init__(self, table_number):
@@ -12,40 +11,28 @@ class PoolTable:
         self.end_time = None
 
 
-def occupancy_check():
-    for table in table_list:
-        if table.is_occupied == True:
-            continue
+    def occupancy_check(self):
+        if self.is_occupied == True:
+            print(f"STOP - Pool Table #{self.table_number} is NOT available for play.")
         else:
-            print(f"Pool Table #{table.table_number} is available for play.")
+            print(f"Pool Table #{self.table_number} is available for play.")
 
 
-def assign_table(table):
-    with open("02-10-2021.txt", "a") as table_log:
-        if table.is_occupied == True:
-            print("STOP - This table is occupied.")
-        elif table.is_occupied == False:
-            table.is_occupied = True
-            table.start_strptime = datetime.datetime.now().strptime
-            table.start_strftime = datetime.datetime.now().strftime("%I:%M %p on %m/%d/%Y")
-            table_log.write(f"Pool Table #{table.table_number} in use; beginning at {table.start_strftime}\n")  
-            print(f"Pool Table #{table.table_number} in use; beginning at {table.start_strftime}")
+    def assign_table(self):
+        with open("02-11-2021.txt", "a") as table_log:
+            if self.is_occupied == True:
+                print("STOP - This table is occupied.")
+            elif self.is_occupied == False:
+                self.is_occupied = True
+                self.start_strptime = datetime.datetime.now().strptime
+                self.start_strftime = datetime.datetime.now().strftime("%I:%M %p on %m/%d/%Y")
+                table = {"table_number" : self.table_number), "is_occupied": self.is_occupied, "self.start_strftime": self.start_strftime}
+                print(table)
+                #table_log.write(f"Pool Table #{table.table_number} in use; beginning at {table.start_strftime}\n")  
+                # print(f"Pool Table #{table.table_number} in use; beginning at {table.start_strftime}")
         
+# def close_out_table(self):
 
-t_one = PoolTable(1)
-t_two = PoolTable(2)
-t_three = PoolTable(3)
-t_four = PoolTable(4)
-t_five = PoolTable(5)
-t_six = PoolTable(6)
-t_seven = PoolTable(7)
-t_eight = PoolTable(8)
-t_nine = PoolTable(9)
-t_ten = PoolTable(10)
-t_eleven = PoolTable(11)
-t_twelve = PoolTable(12)
-
-start_list = [t_one, t_two, t_three, t_four, t_five, t_six, t_seven, t_eight, t_nine, t_ten, t_eleven, t_twelve]
 
 # #Business Logic
 
--- pool_table_main.py
+++ pool_table_main.py
@@ -1,2 +1,16 @@
 #WEEK 2 POOL TABLE PROJECT
-#USER INPUT CODE GOES HERE 
+#OBJECT CREATION AND USER INPUT CODE GOES HERE 
+
+from pool_table_class import PoolTable
+
+
+pool_tables = []
+
+for index in range(13):
+    pool_table = PoolTable(index+1)
+    pool_tables.append(pool_table)
+
+
+
+pt_one = pool_tables[0]
+pt_one.assign_table()
