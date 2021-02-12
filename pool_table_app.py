a3d6a2e5720021086224b1c48513be36ba2c39bc
naldurien <nicole.aldurien@gmail.com>
2021-02-12 16:20:08 -0500

    works, current time played added to 'view all tables' list

--- pool_table_app.py
+++ pool_table_app.py
@@ -1,6 +1,5 @@
 #WEEK 2 DAYS 3-5: POOL TABLE APP
-#It works and writes to json, recognized by json.lint as valid. Whether it's capturing
-# the correct info, and whether I can read back out of it, is to be determined tomorrow.
+
 
 import json
 import datetime
@@ -8,12 +7,14 @@ now = datetime.datetime.now()
 today = now.date()
 tables = []
 
+
 class PoolTable:
     def __init__(self, ident):
         self.ident = ident
         self.is_occupied = False
         self.start_time = datetime.datetime.now()
         self.end_time = datetime.datetime.now()
+        self.time_played = 0
 
     
     def check_out_to_players(self):
@@ -32,13 +33,14 @@ class PoolTable:
         print(f"\nPlay ended at Table #{str(self.ident)} on {end_timestamp}")
         
 
-    def total_time_played(self):
+    def total_playtime(self):
         delta = self.end_time - self.start_time
+        self.time_played = delta
         seconds = delta.seconds
         hours = seconds // 3600
         remaining_secs = seconds - (hours * 3600)
         minutes = remaining_secs // 60
-        print('\nTotal Time Played: {:02} hours, {:02} minutes'.format(int(hours), int(minutes)))
+        print('\nTotal Time Played: {:02} hours, {:02} minutes\n'.format(int(hours), int(minutes)))
 
 
 # MAKE TIME PRETTY
@@ -48,16 +50,19 @@ def format_time(time):
     return str_timestamp
 
 
-
 #OCCUPANCY CHECK
 def occupancy_check():
     print("\n- - - - - Table List - - - - -\n")
     for table in tables:
         if table.is_occupied == True:
-            print(f"Table #{table.ident} - OCCUPIED")
+            start = format_time(table.start_time)
+            table.end_time = datetime.datetime.now()
+            print(f"Table #{table.ident} - OCCUPIED - Play started on {start}.")
+            table.total_playtime()
         else:
-            print(f"Table #{table.ident} - VACANT")
+            print(f"\nTable #{table.ident} - VACANT")
     print("\n")
+
 
 # GIVE EMPTY TABLE TO PLAYERS
 def assign_table():
@@ -77,8 +82,8 @@ def close_out_table():
     pick = int(input("Please select an occupied table to close out: "))
     chosen = tables[pick-1]
     chosen.return_from_use()
+    chosen.total_playtime()
     morph_for_json(tables)
-    chosen.total_time_played()
 
 
 # Morph List of Table Objects to List of Dictionaries with Object Values, for Writing To a File:
@@ -88,12 +93,14 @@ def morph_for_json(list):
         int_ident = int(table.ident)
         bool_occupancy = bool(table.is_occupied)
         str_start = str(table.start_time)
-        str_end = str(table.end_time)  
+        str_end = str(table.end_time)
+        str_total = str(table.total_playtime)  
         table_dict = {
             "ident": int_ident, 
             "is_occupied": bool_occupancy, 
             "start_time": str_start, 
-            "end_time": str_end
+            "end_time": str_end,
+            "total_playtime": str_total
             }
         dictionary_list.append(table_dict)
     with open (f"{today}.json", "w") as file_object:
@@ -110,12 +117,12 @@ for index in range(12):
 # MENU ESTA AQUI
 while True:
     print("\n- - - - - POOL HALL OVERSIGHT MENU - - - - -")
-    choice = input("Press 1 to assign a table.\nPress 2 to close out a table.\nPress q to quit.\n\nYour selection: ")
+    choice = input("""\nPress 1 to assign a table.\nPress 2 to close out a table.\nPress 3 to view all tables.\nPress q to quit.\n\nYour selection: """)
     if choice == "1":
         assign_table()
     if choice == "2":
         close_out_table()
+    if choice == "3":
+        occupancy_check()
     if choice == "q":
         break
-    else:
-        print("\nPlease select an option from the menu: ")
